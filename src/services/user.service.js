6efe66a8fda576a000931eccd3ca7232e4d536b7
PradeepVishwakarma <pradeepvish1213@gmail.com>
2024-04-27 16:31:54 +0530

    Project Created

---
+++ src/services/user.service.js
@@ -0,0 +1,97 @@
+const httpStatus = require('http-status');
+const { Op } = require('sequelize');
+const { User } = require('../models');
+const ApiError = require('../utils/ApiError');
+
+const isEmailTaken = async (email, userId) => {
+    const user = await User.findOne({ where: { email, id: { [Op.ne]: userId } } });
+    return !!user;
+};
+/**
+ * Get user by email
+ * @param {string} email
+ * @returns {Promise<User>}
+ */
+const getUserByEmail = async (email) => {
+    return User.findOne({ where: { email } });
+};
+
+/**
+ * Create a user
+ * @param {Object} userBody
+ * @returns {Promise<User>}
+ */
+const createUser = async (userBody) => {
+    if (await getUserByEmail(userBody.email)) {
+        throw new ApiError(httpStatus.BAD_REQUEST, 'Email already taken');
+    }
+    return User.create(userBody);
+};
+
+const paginateUsers = async (filter, options, conditions) => {
+    return User.findAll({ ...filter, ...options, where: conditions });
+};
+/**
+ * Query for users
+ * @param {Object} filter - Sequelize filter
+ * @param {Object} options - Query options
+ * @param {Object} conditions - Query where column
+ * @param {string} [options.sortBy] - Sort option in the format: ['field1','ASC'],['field2','DESC']
+ * @param {number} [options.limit] - Maximum number of results per page (default = 10)
+ * @param {number} [options.page] - Current page (default = 1)
+ * @returns {[Promise<[]>]}
+ */
+const queryUsers = async (filter, options, conditions) => {
+    return paginateUsers(filter, options, conditions);
+};
+
+/**
+ * Get user by id
+ * @param {UUID} id
+ * @returns {Promise<User>}
+ */
+const getUserById = async (id) => {
+    return User.findByPk(id);
+};
+
+/**
+ * Update user by id
+ * @param {UUID} userId
+ * @param {Object} updateBody
+ * @returns {Promise<User>}
+ */
+const updateUserById = async (userId, updateBody) => {
+    const user = await getUserById(userId);
+    if (!user) {
+        throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
+    }
+    if (updateBody.email && (await isEmailTaken(updateBody.email, userId))) {
+        throw new ApiError(httpStatus.BAD_REQUEST, 'Email already taken');
+    }
+    Object.assign(user, updateBody);
+    await user.save();
+    return user;
+};
+
+/**
+ * Delete user by id
+ * @param {UUID} userId
+ * @returns {Promise<User>}
+ */
+const deleteUserById = async (userId) => {
+    const user = await getUserById(userId);
+    if (!user) {
+        throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
+    }
+    await User.destroy({ where: { id: userId } });
+    return user;
+};
+
+module.exports = {
+    createUser,
+    queryUsers,
+    getUserById,
+    getUserByEmail,
+    updateUserById,
+    deleteUserById,
+};
